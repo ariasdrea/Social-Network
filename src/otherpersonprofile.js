ef242eabeb64de5aa7815279ad0a699e50fbc2dd
Andrea Arias <ariasdrea@gmail.com>
2018-12-05 17:08:39 +0100

    committing

--- src/otherpersonprofile.js
+++ src/otherpersonprofile.js
@@ -31,7 +31,6 @@ export default class OtherPersonProfile extends React.Component {
     render() {
         return (
             <div>
-                <h1> {this.props.match.params.id} </h1>
                 <div className="profile-container">
                     <img
                         className="pp"
