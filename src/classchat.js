047d84d242bb74fe744cf9e363ac755c69e144b0
Andrea Arias <ariasdrea@gmail.com>
2019-08-07 09:19:58 +0200

    adding demo from encounter

---
+++ src/classchat.js
@@ -0,0 +1,18 @@
+// Pete's demo of building a chat room
+// import React, { useEffect, useRef } from "react";
+// //this allows us to emit stuff
+// import { socket } from "./socket";
+// //allows us to use redux
+// import { useSelector } from "react-redux";
+//
+// export default function Chat() {
+//     const chatMessages = useSelector(state => state && state.chatMessage);
+//
+//     console.log("here are my last 10 messages: ", chatMessages);
+//
+//     return (
+//         <div className="chat">
+//             <h1>Chat Room</h1>
+//         </div>
+//     );
+// }
