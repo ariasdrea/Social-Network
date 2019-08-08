16b58484887138ff8c9f34700ca926fa4f6b92c6
Andrea Arias <ariasdrea@gmail.com>
2019-08-08 13:25:13 +0200

    updated react and dom versions in order to use hooks

--- bundle-server.js
+++ bundle-server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
-const ca = require("chalk-animation");
 
 app.use(require("./build.js"));
 
-app.listen(8081, () => ca.rainbow(`Ready to compile and serve bundle.js`));
+app.listen(8081, () => console.log(`Ready to compile and serve bundle.js`));
--- src/profile.js
+++ src/profile.js
@@ -1,22 +1,15 @@
 import React from "react";
 import ProfilePic from "./profilepic";
 import Bio from "./bio";
-import EasyTransition from "react-easy-transition";
 
 export default function Profile(props) {
     return (
         <div className="profile-container">
             <p className="profile-title"> Profile && Biography </p>
             <div className="test">
-                <EasyTransition
-                    initialStyle={{ opacity: 0 }}
-                    transition="opacity 2s ease-in"
-                    finalStyle={{ opacity: 1 }}
-                >
-                    <p className="profile-welcome-msg">
-                        Welcome {props.first}, feel free to explore!{" "}
-                    </p>
-                </EasyTransition>
+                <p className="profile-welcome-msg">
+                    Welcome {props.first}, feel free to explore!{" "}
+                </p>
                 <ProfilePic
                     profilePicUrl={props.profilePicUrl || "quest.png"}
                     showUploader={props.showUploader}
