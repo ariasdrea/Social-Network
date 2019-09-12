7e4723e190899680b083c22232495b4c1db7e755
Andrea Arias <ariasdrea@gmail.com>
2019-09-12 16:55:16 +0200

    completed find people functionality - when they delete search

--- src/findPeople.js
+++ src/findPeople.js
@@ -4,48 +4,55 @@ import axios from "./axios";
 export default function FindPeople() {
     const [users, setUsers] = useState([]);
     const [val, setVal] = useState();
+    const [error, setError] = useState();
 
     useEffect(() => {
-        axios.get("/getUsers").then(({data}) => {
+        (async () => {
+            const { data } = await axios.get('/getUsers');
             setUsers(data);
-        });
-    }, []);
+        })();
+    }, [val]);
 
     useEffect(() => {
         if (!val) {
             return;
         }
 
-        axios.get(`/searchUsers/${val}`).then(({data}) => {
-            setUsers(data);
-        });
-    }, [val]);
+        (async () => {
+            console.log('val in async effect #2: ', val);
+            const { data } = await axios.get(`/searchUsers/${val}`);
+
+            if (!data.length) {
+                setError('Sorry, there are no matching results');
+            } else {
+                setError(false);
+                setUsers(data);
+            }
+
+        })();
+    }, [val, error]);
 
     return (
         <div className='search-container'>
             <input className='find-people-search' onChange={e => setVal(e.target.value)} />
-            <p></p>
-            <p>Recently joined users </p>
-            <div className="last-joined-container">
-                {users.map(each => (
-                    <div className="each-user" key={each.id}>
-                        <p>{each.first}</p>
-                        <img
-                            className="each-user-img"
-                            src={each.profilepicurl || "quest.png"}
-                        />
+            {error ? (
+                <p className='search-error'>{error}</p>
+            ) : (
+                <div>
+                    <p>Recently joined users </p>
+                    <div className="last-joined-container">
+                        {users.map(each => (
+                            <div className="each-user" key={each.id}>
+                                <p>{each.first}</p>
+                                <img
+                                    className="each-user-img"
+                                    src={each.profilepicurl || "quest.png"}
+                                />
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
+                </div>
+            )}
         </div>
     );
 }
-
-
-
-// this goes and gets a new list of users
-// write a query to get these users
-// this triggers the state change where you're changing the value of text
-// useEffect runs again when value in input field changes makes another request to get the latest  - use return funciton here to manage out of order
-// set the new list of users
-//different routes is good - 2 distinct queries. one happens when the component moutns, another one happens when the state changes
